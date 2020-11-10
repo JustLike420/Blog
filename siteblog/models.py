4709a79616605fd681c46c001314309415037f35
JustLike420 <justlikevova@gmail.com>
2020-11-10 16:53:48 +0300

    create db

--- siteblog/models.py
+++ siteblog/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
 # Create your models here.
+'''
+Category
+========
+title, slug
+ 
+Tag
+========
+title, slug 
+ 
+Post
+========
+title, slug, author, content, created_at, photo, views, category, tags 
+'''
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, verbose_name='url', unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']  # сортирока по имяни в алф порядке
+
+class Tag(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, verbose_name='url', unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']  # сортирока по имяни в алф порядке
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, verbose_name='url', unique=True)
+    author = models.CharField(max_length=100)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Опубликовано')
+    photo = models.ImageField(blank=True, default='photo.png')
+    views = models.IntegerField(default=0, verbose_name='Кол-во просмотров')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts')
+    tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-created_at']  # сортирока по дате в обратном порядке свежие выше
